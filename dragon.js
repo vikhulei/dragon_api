10bfe636785a82be7f84b9ab1e18683155a68c20
vikhulei <vikhulei@gmail.com>
2022-10-24 20:59:12 +0100

    dragon traits added

--- dragon.js
+++ dragon.js
@@ -1,14 +1,31 @@
+const TRAITS = require("./traits")
+
 const INITIAL_VALUES = {
    nickname: "unnamed",
    get birthday()  {
       return new Date()
+   },
+   get randomTraits() {
+      const traits = [];
+
+      TRAITS.forEach(TRAIT => {
+         const traitType = TRAIT.type;
+         const traitValues = TRAIT.values;
+
+         const traitValue = traitValues[
+            Math.floor(Math.random() * traitValues.length)
+         ]
+         traits.push({traitType, traitValue})
+      })
+      return traits
    }
 }
 
 class Dragon {
-     constructor({birthday, nickname} = {}) {
+     constructor({birthday, nickname, traits} = {}) {
         this.birthday = birthday || INITIAL_VALUES.birthday
         this.nickname = nickname || INITIAL_VALUES.nickname
+        this.traits = traits || INITIAL_VALUES.randomTraits
      }
 }
 
--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const Dragon = require("./dragon")
 
 const fooey = new Dragon({birthday: new Date(), nickname: "fooey"})
 const baloo = new Dragon({birthday: new Date(), nickname: "baloo"})
-
 const mimar = new Dragon()
-
 console.log("fooey", fooey)
 console.log("baloo", baloo)
 console.log("mimar", mimar)
+
 setTimeout(() => {
     const gooby = new Dragon()
     console.log("gooby", gooby)
 }, 3000)
-
