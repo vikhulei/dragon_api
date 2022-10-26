962cbb0c4e5b6dc706318986ec4d7544a84e7772
vikhulei <vikhulei@gmail.com>
2022-10-26 23:47:00 +0100

    experimenting with traits object

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Dragon = require("./dragon")
+const traits = require("./traits.json")
 
 const fooey = new Dragon({birthday: new Date(), nickname: "fooey"})
 const baloo = new Dragon({birthday: new Date(), nickname: "baloo"})
 const mimar = new Dragon()
-console.log("fooey", fooey)
-console.log("baloo", baloo)
-console.log("mimar", mimar)
+// console.log("fooey", fooey)
+// console.log("baloo", baloo)
+// console.log("mimar", mimar)
 
-setTimeout(() => {
-    const gooby = new Dragon()
-    console.log("gooby", gooby)
-}, 3000)
+// setTimeout(() => {
+//     const gooby = new Dragon()
+//     console.log("gooby", gooby)
+// }, 3000)
+
+console.log(traits[0])
