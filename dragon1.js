6a6357a9f2bea9e588e3f7302a3191171a314c4a
vikhulei <vikhulei@gmail.com>
2022-11-04 11:33:56 +0000

    test files deleted

--- dragon1.js
+++
@@ -1,8 +0,0 @@
-class Dragon {
-    constructor(birthday, nickname) {
-        this.birthday = birthday
-        this.nickname = nickname
-    }
-}
-
-module.exports = Dragon
--- index1.js
+++
@@ -1,7 +0,0 @@
-const Dragon = require("./dragon1")
-
-const bugy = new Dragon("12/03/99", "bugy")
-const hugy = new Dragon("01/01/00", "hugy")
-
-console.log(bugy)
-console.log(hugy)
