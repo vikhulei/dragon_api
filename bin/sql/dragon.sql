ac0597719f023cf80ca2df56d2025f5854668240
vikhulei <vikhulei@gmail.com>
2022-11-07 18:53:08 +0000

    adding pool object

--- bin/sql/dragon.sql
+++ bin/sql/dragon.sql
@@ -1,6 +1,6 @@
 CREATE TABLE dragon (
     id              SERIAL PRIMARY KEY,
-    birthday        TIMESTAMP NO NULL,
+    birthday        TIMESTAMP NOT NULL,
     nickname        VARCHAR(64),
     "generationId"    INTEGER,
     FOREIGN KEY ("generationId") REFERENCES generation(id)
