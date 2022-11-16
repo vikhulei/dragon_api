e2b0fd3fb2d807e9373e913a39479c9a20fe7fde
vikhulei <vikhulei@gmail.com>
2022-11-16 14:56:43 +0000

    adding records to DragonTrait table

--- app/dragon/table.js
+++ app/dragon/table.js
@@ -1,4 +1,5 @@
 const pool = require("../../databasePool")
+const DragonTraitTable = require("../dragonTrait/table")
 
 class DragonTable {
     static storeDragon(dragon) {
@@ -11,7 +12,14 @@ class DragonTable {
             (error, response) => {
                 if(error) return reject(error)
                 const dragonId = response.rows[0].id
-                resolve({dragonId})
+
+                Promise.all(dragon.traits.map(({traitType, traitValue}) => {
+                    return DragonTraitTable.storeDragonTrait({
+                        dragonId, traitType, traitValue
+                    })
+                }))
+                .then(() => resolve({dragonId}))
+                .catch(error => reject(error)) 
             }
             )
         })
